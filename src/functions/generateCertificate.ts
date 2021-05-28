45f213d0a9cb73d76f112e30e437c8110bb96318
tharlys10 <tharlys10c@hotmail.com>
2021-05-28 14:19:59 -0300

    feat: :sparkles: genereted pdf

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -58,6 +58,27 @@ export const handle = async (event) => {
 
   // Transformar PDF
 
+  const browser = await chromium.puppeteer.launch({
+    headless: true,
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath
+  });
+
+  const page = await browser.newPage();
+
+  await page.setContent(content);
+
+  const pdf = await page.pdf({
+    format: "a4",
+    landscape: true,
+    path: process.env.IS_OFFLINE ? "certificate.pdf" : null,
+    printBackground: true,
+    preferCSSPageSize: true
+  });
+
+  await browser.close();
+
   // Salvar no S3
 
   return {
